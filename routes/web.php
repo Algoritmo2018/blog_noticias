18541fc719a3913e1370b66176a162c502dbd54a
Luis Chilembo Mateus <luischilembomateus@gmail.com>
2024-02-21 14:54:45 +0100

    autenticação e recuperação de senha por email recebido

--- routes/web.php
+++ routes/web.php
@@ -78,3 +78,17 @@ Route::get('/article/category', [HomeController::class, 'index'])->name('article
 Route::get('/homeadm/article/title', [AdminController::class, 'HomeAdm'])->name('article.search');
 
 Route::put('/{id}/article/update', [ArticleController::class, 'UpdateArticle'])->name('update_article');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
