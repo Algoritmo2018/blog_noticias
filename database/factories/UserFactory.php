13509802f558314933115c4f8e853baa10ffdc95
Luis Chilembo Mateus <luischilembomateus@gmail.com>
2024-07-30 20:05:08 +0100

    melhorias feitas

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'is_admin' => 1,
+            'password' => bcrypt('123456789'), // password
             'remember_token' => Str::random(10),
         ];
     }
